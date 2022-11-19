44b652e603e652e379ae43e0b98045f25e6f4975
KaliNinB <109882612+KaliNinB@users.noreply.github.com>
2022-11-19 21:24:36 +0200

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Владислав Калинин on 19.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
